eb2d39f681da5bf9c67051f5184ba9cf34aaa4be
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-04-10 14:04:52 +1000

    fix: update run-ddb to include redis

--- run-ddb.sh
+++ run-ddb.sh
@@ -2,9 +2,16 @@ docker run -p 8765:8765 amazon/dynamodb-local -jar DynamoDBLocal.jar -port 8765 
 
 DYNAMODB_PID=$!
 
+# run Redis
+docker run --name brawlers-redis -p 4515:6379 -d redis &
+
+REDIS_PID=$!
+
 cleanup() {
-  echo "Stopping DynamoDB Local..."
-  docker stop $(docker ps -q --filter ancestor=amazon/dynamodb-local) &>/dev/null
+    echo "Stopping DynamoDB Local..."
+    docker stop $(docker ps -q --filter ancestor=amazon/dynamodb-local) &>/dev/null
+    echo "Stopping Redis..."
+    docker stop $(docker ps -q --filter ancestor=redis) &>/dev/null
 }
 
 trap cleanup INT
@@ -12,12 +19,12 @@ trap cleanup INT
 sleep 5
 
 aws dynamodb create-table \
-   --table-name cashier-events-local \
-   --attribute-definitions AttributeName=aggregateId,AttributeType=S AttributeName=version,AttributeType=N AttributeName=eventStoreId,AttributeType=S AttributeName=timestamp,AttributeType=S \
-   --key-schema AttributeName=aggregateId,KeyType=HASH AttributeName=version,KeyType=RANGE \
-   --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=5 \
-   --global-secondary-indexes \
-        "[
+    --table-name cashier-events-local \
+    --attribute-definitions AttributeName=aggregateId,AttributeType=S AttributeName=version,AttributeType=N AttributeName=eventStoreId,AttributeType=S AttributeName=timestamp,AttributeType=S \
+    --key-schema AttributeName=aggregateId,KeyType=HASH AttributeName=version,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=5 \
+    --global-secondary-indexes \
+    "[
             {
                 \"IndexName\": \"initialEvents\",
                 \"KeySchema\": [{\"AttributeName\":\"eventStoreId\",\"KeyType\":\"HASH\"},
@@ -31,7 +38,7 @@ aws dynamodb create-table \
                 }
             }
         ]" \
-   --endpoint-url http://localhost:8765 \
-   --no-cli-pager
+    --endpoint-url http://localhost:8765 \
+    --no-cli-pager
 
 wait $DYNAMODB_PID
