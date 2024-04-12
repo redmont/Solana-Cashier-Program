6483834eb9696fd7b1ec48ff1a0dcf6ce9a917a8
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-04-12 18:19:46 +0930

    Scaffolded BetPlacementPanel

--- apps/ui/src/app/page.tsx
+++ apps/ui/src/app/page.tsx
@@ -1,13 +1,24 @@
-import { EthConnectButton } from '@/components/web3';
+'use client';
+
+import { EthConnectButton, useEthWallet } from '@/components/web3';
+import { BetInputPanel } from '@/components/betPlacementPanel';
 
 export default function Home() {
+  const { isReady, isConnected } = useEthWallet();
+
   return (
     <div className="main-page">
       <div className="match-stream-container">
         <img className="stream-placeholder" src="/match.png" />
       </div>
-      <div className="match-betting-panel">
-        <EthConnectButton size="large" />
+      <div className="match-bet-container">
+        {isReady && !isConnected && (
+          <div className="connect-wallet-panel">
+            <EthConnectButton size="large" />
+          </div>
+        )}
+
+        {isReady && isConnected && <BetInputPanel />}
       </div>
     </div>
   );
