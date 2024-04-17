902d5a11706b6e4d7e644130e61f58bf567a7494
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-04-17 18:58:32 +0930

    Move widgets from Layout to a main page

--- apps/ui/src/app/page.tsx
+++ apps/ui/src/app/page.tsx
@@ -5,6 +5,11 @@ import { EthConnectButton, useEthWallet } from '@/components/web3';
 import { BetPlacementWidget } from '@/components/betPlacementWidget';
 import { CurrentBetWidget } from '@/components/currentBetWidget';
 import { HistoricalBetPanel } from '@/components/historicalBetPanel';
+import { twitchChannel } from '@/config';
+import { BetListWidget } from '@/components/betListWidget';
+import { StreamChat } from '@/components/streamChat';
+import { ActivityStream } from '@/components/activityStream';
+import { TwitchChat } from 'react-twitch-embed';
 
 export default function Home() {
   const { isReady, isConnected } = useEthWallet();
@@ -12,25 +17,43 @@ export default function Home() {
   const { currentBets } = useAppState();
 
   return (
-    <div className="main-page">
-      <div className="match-stream-container">
-        <img className="stream-placeholder" src="/match.png" />
+    <main className="flex flex-grow-1">
+      <div className="layout-left-sidebar">
+        <BetListWidget />
       </div>
 
-      <div className="match-bet-container">
-        {isReady && !isConnected && (
-          <div className="connect-wallet-panel">
-            <EthConnectButton size="large" />
+      <div className="layout-content">
+        <div className="main-page">
+          <div className="match-stream-container">
+            <img className="stream-placeholder" src="/match.png" />
           </div>
-        )}
 
-        {isReady && isConnected && currentBets && <CurrentBetWidget />}
+          <div className="match-bet-container">
+            {isReady && !isConnected && (
+              <div className="connect-wallet-panel">
+                <EthConnectButton size="large" />
+              </div>
+            )}
 
-        {isReady && isConnected && (
-          <BetPlacementWidget compact={!!currentBets} />
-        )}
-        {/* {isReady && isConnected && <HistoricalBetPanel />} */}
+            {isReady && isConnected && currentBets && <CurrentBetWidget />}
+
+            {isReady && isConnected && (
+              <BetPlacementWidget compact={!!currentBets} />
+            )}
+            {/* {isReady && isConnected && <HistoricalBetPanel />} */}
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div className="layout-right-sidebar">
+        <div className="activity-stream-container">
+          <ActivityStream />
+        </div>
+
+        <div className="stream-chat-container">
+          <TwitchChat channel={twitchChannel} width="100%" height="100%" />
+        </div>
+      </div>
+    </main>
   );
 }
--- apps/ui/src/components/layout/Layout.tsx
+++ apps/ui/src/components/layout/Layout.tsx
@@ -7,11 +7,6 @@ import { AppStateProvider } from '../AppStateProvider';
 
 import { EthConnectButton, EthMobileConnectButton } from '@/components/web3';
 import { ChildContainerProps } from '@/types';
-import { twitchChannel } from '@/config';
-import { BetListWidget } from '@/components/betListWidget';
-import { StreamChat } from '@/components/streamChat';
-import { ActivityStream } from '@/components/activityStream';
-import { TwitchChat } from 'react-twitch-embed';
 
 export const Layout = (props: ChildContainerProps) => {
   return (
@@ -44,28 +39,6 @@ export const Layout = (props: ChildContainerProps) => {
                 <EthMobileConnectButton className="md:hidden" />
               </div>
             </div>
-
-            <main className="flex flex-grow-1">
-              <div className="layout-left-sidebar">
-                <BetListWidget />
-              </div>
-
-              <div className="layout-content">{props.children}</div>
-
-              <div className="layout-right-sidebar">
-                <div className="activity-stream-container">
-                  <ActivityStream />
-                </div>
-
-                <div className="stream-chat-container">
-                  <TwitchChat
-                    channel={twitchChannel}
-                    width="100%"
-                    height="100%"
-                  />
-                </div>
-              </div>
-            </main>
           </div>
         </AppStateProvider>
       </AuthProvider>
