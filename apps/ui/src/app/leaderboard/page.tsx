b02c095a9bd522d6fbb683e41a42176499991c55
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-05-09 19:55:41 +0930

    Bind rank to leaderboard record

--- apps/ui/src/app/leaderboard/page.tsx
+++ apps/ui/src/app/leaderboard/page.tsx
@@ -12,13 +12,15 @@ import {
 import { useEthWallet, useSocket } from '@/hooks';
 import { truncateEthAddress } from '@/utils';
 
-interface LeaderboardRecord {
+interface RecordProps {
   walletAddress: string;
   balance: string;
+  rank: number;
+  highlighted?: boolean;
 }
 
 export default function Leaderboard() {
-  const [records, setRecords] = useState<LeaderboardRecord[]>([]);
+  const [records, setRecords] = useState<RecordProps[]>([]);
   const { send, connected } = useSocket();
   const { address } = useEthWallet();
 
@@ -50,36 +52,13 @@ export default function Leaderboard() {
             <div className="empty-state">No records yet</div>
           )}
 
-          {records.map(({ walletAddress, balance }) => (
-            <div
+          {records.map(({ walletAddress, ...props }) => (
+            <MobileRecord
+              {...props}
               key={walletAddress}
-              className={classNames('mobile-record', {
-                highlighted: address === walletAddress,
-              })}
-            >
-              <div className="mobile-record-body">
-                <div className="rank">1</div>
-                <div className="player">
-                  {truncateEthAddress(walletAddress)}
-                </div>
-                <div className="points-label">Points:</div>
-                <div className="points-value">{balance}</div>
-                <div className="winrate-label">Winrate:</div>
-                <div className="winrate-value">96%</div>
-              </div>
-
-              <div className="mobile-record-footer">
-                <div className="stat wins">
-                  <span className="stat-label">Wins:</span>
-                  <span className="stat-value">45</span>
-                </div>
-
-                <div className="stat losses">
-                  <span className="stat-label">Losses:</span>
-                  <span className="stat-value">2</span>
-                </div>
-              </div>
-            </div>
+              walletAddress={walletAddress}
+              highlighted={walletAddress === address}
+            />
           ))}
 
           {records.length > 0 && (
@@ -94,17 +73,13 @@ export default function Leaderboard() {
               </div>
 
               <div className="table-body">
-                {records.map(({ walletAddress, balance }) => (
-                  <div className="table-row" key={walletAddress}>
-                    <div className="rank">1</div>
-                    <div className="player">
-                      {truncateEthAddress(walletAddress)}
-                    </div>
-                    <div className="wins">45</div>
-                    <div className="losses">2</div>
-                    <div className="winrate">96%</div>
-                    <div className="points">{balance}</div>
-                  </div>
+                {records.map(({ walletAddress, ...props }) => (
+                  <TableRow
+                    {...props}
+                    key={walletAddress}
+                    walletAddress={walletAddress}
+                    highlighted={walletAddress === address}
+                  />
                 ))}
               </div>
             </ScrollPanel>
@@ -114,3 +89,46 @@ export default function Leaderboard() {
     </main>
   );
 }
+
+const MobileRecord: FC<RecordProps> = (props) => (
+  <div
+    className={classNames('mobile-record', {
+      highlighted: props.highlighted,
+    })}
+  >
+    <div className="mobile-record-body">
+      <div className="rank">{props.rank}</div>
+      <div className="player">{truncateEthAddress(props.walletAddress)}</div>
+      <div className="points-label">Points:</div>
+      <div className="points-value">{props.balance}</div>
+      <div className="winrate-label">Winrate:</div>
+      <div className="winrate-value">96%</div>
+    </div>
+
+    <div className="mobile-record-footer">
+      <div className="stat wins">
+        <span className="stat-label">Wins:</span>
+        <span className="stat-value">45</span>
+      </div>
+
+      <div className="stat losses">
+        <span className="stat-label">Losses:</span>
+        <span className="stat-value">2</span>
+      </div>
+    </div>
+  </div>
+);
+
+const TableRow: FC<RecordProps> = (props) => (
+  <div
+    className={classNames('table-row', { highlighted: props.highlighted })}
+    key={props.walletAddress}
+  >
+    <div className="rank">{props.rank}</div>
+    <div className="player">{truncateEthAddress(props.walletAddress)}</div>
+    <div className="wins">45</div>
+    <div className="losses">2</div>
+    <div className="winrate">96%</div>
+    <div className="points">{props.balance}</div>
+  </div>
+);
