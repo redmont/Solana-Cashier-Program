072934bd7c2326d63e1191e7224ab989a35c1ddd
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-06-03 00:05:24 +0930

    Added Slider component

--- apps/ui/src/components/connectWalletWidget/ConnectWalletWidget.tsx
+++ apps/ui/src/components/connectWalletWidget/ConnectWalletWidget.tsx
@@ -1,11 +1,14 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { JoinButton } from '@/components/JoinButton';
+import { Slider } from '@/components/slider';
 
 export const ConnectWalletWidget: FC = () => {
+  const [val, setVal] = useState(20);
   return (
     <div className="widget connect-wallet-widget">
       <div className="widget-body framed">
-        <JoinButton size="large" />
+        {/* <JoinButton size="large" /> */}
+        <Slider value={val} onValueChange={setVal} />
       </div>
     </div>
   );
