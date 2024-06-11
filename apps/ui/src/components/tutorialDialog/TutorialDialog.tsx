7ff1f4c69692533a0ac243a36be10ee6a5a7da2a
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-06-11 23:18:50 +0930

    Add mobile layout and transition to desktop one

--- apps/ui/src/components/tutorialDialog/TutorialDialog.tsx
+++ apps/ui/src/components/tutorialDialog/TutorialDialog.tsx
@@ -3,6 +3,7 @@ import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button';
 import { Carousel } from 'primereact/carousel';
 import { useDynamicContext } from '@dynamic-labs/sdk-react-core';
+import { classNames } from 'primereact/utils';
 
 export interface TutorialDialogProps {
   visible: boolean;
@@ -149,42 +150,42 @@ export const TutorialDialog: FC<TutorialDialogProps> = ({
       </div>
 
       <div className="dialog-footer">
-        <div className="dialog-footer-start">
+        <div className="dialog-nav">
+          {slideNum > 0 && (
+            <Button
+              icon="pi pi-arrow-left"
+              label="Back"
+              className="button-back p-button-secondary p-button-outlined border-none"
+              onClick={goBack}
+            ></Button>
+          )}
+
           {!isLastSlide && (
             <Button
-              className="p-button-secondary p-button-outlined border-none text-400"
-              label="I know all of this"
-              onClick={dismiss}
-            />
+              icon="pi pi-arrow-right"
+              iconPos="right"
+              label="Next"
+              className="button-next p-button-secondary p-button-outlined"
+              onClick={goNext}
+            ></Button>
+          )}
+
+          {isLastSlide && (
+            <Button
+              label="Let's Go!"
+              className="p-button-secondary p-button-outlined"
+              onClick={complete}
+            ></Button>
           )}
         </div>
 
-        {slideNum > 0 && (
-          <Button
-            icon="pi pi-arrow-left"
-            label="Back"
-            className="p-button-secondary p-button-outlined border-none"
-            onClick={goBack}
-          ></Button>
-        )}
-
-        {!isLastSlide && (
-          <Button
-            icon="pi pi-arrow-right"
-            iconPos="right"
-            label="Next"
-            className="p-button-secondary p-button-outlined"
-            onClick={goNext}
-          ></Button>
-        )}
-
-        {isLastSlide && (
-          <Button
-            label="Let's Go!"
-            className="p-button-secondary p-button-outlined"
-            onClick={complete}
-          ></Button>
-        )}
+        <Button
+          className={classNames(
+            'button-complete p-button-secondary p-button-outlined border-none text-400',
+          )}
+          label="I know all of this"
+          onClick={dismiss}
+        />
       </div>
     </Dialog>
   );
