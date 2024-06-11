6bbce900ecf094bef5df9c862cd8bd1e61ad9c83
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-06-12 00:30:30 +0930

    Split TutorialDialog file

--- apps/ui/src/components/tutorialDialog/TutorialDialog.tsx
+++ apps/ui/src/components/tutorialDialog/TutorialDialog.tsx
@@ -1,93 +1,17 @@
-import { FC, ReactNode, useCallback, useState } from 'react';
+import { FC, useCallback, useState } from 'react';
 import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button';
 import { Carousel } from 'primereact/carousel';
 import { useDynamicContext } from '@dynamic-labs/sdk-react-core';
 import { classNames } from 'primereact/utils';
 
+import { TutorialSlide } from './TutorialSlide';
+import { slides } from './slides';
+
 export interface TutorialDialogProps {
   visible: boolean;
   onHide: () => void;
 }
-
-const slides: TutorialSlideProps[] = [
-  {
-    id: 'intro-slide',
-    image: '/tutorial/welcome.png',
-    text: (
-      <>
-        Be the first to brawl your way to glory.
-        <br /> Welcome to the BRAWL3RS alpha version.
-      </>
-    ),
-  },
-  {
-    id: 'connect-slide',
-    title: 'Join the Fight',
-    image: '/tutorial/connect.png',
-    text: 'Get 1,000 free credits to start Brawling! Login using your email, google, or web3 wallet.',
-  },
-  {
-    id: 'brawlers-slide',
-    title: 'Choose your Brawler(s)',
-    image: '/tutorial/brawlers.png',
-    text: (
-      <>
-        Select the Brawler you think will win! <br />
-        The winner is the real-time coin price with the biggest gain!
-      </>
-    ),
-  },
-  {
-    id: 'stakes-slide',
-    title: 'Place your Stake(s)',
-    image: '/tutorial/stakes.png',
-    text: (
-      <>
-        Decide how many credits to stake on your chosen fighter.
-        <br />
-        You can place multiple stakes on either side.
-      </>
-    ),
-  },
-  {
-    id: 'win-slide',
-    title: 'Watch and win big!',
-    image: '/tutorial/win.png',
-    text: (
-      <>
-        Winning stakes share the total pool of credits from their losing
-        opponent.
-        <br />
-        Share your win on X or stake again!
-      </>
-    ),
-  },
-  {
-    id: 'leaderboard-slide',
-    title: 'Climb the Leaderboard',
-    image: '/tutorial/leaderboard.png',
-    text: (
-      <>
-        Compete in the latest tournament to win real crypto prizes.
-        <br />
-        Stay on top to earn rewards.
-      </>
-    ),
-  },
-  {
-    id: 'community-slide',
-    title: 'Shape the BRAWL3Rs future',
-    image: '/tutorial/community.png',
-    text: (
-      <>
-        Join our community on Discord to share your feedback and shape our
-        direction. Get ready to play with real crypto credits and more coming
-        soon. LFB!
-      </>
-    ),
-  },
-];
 
 export function completeTutorial() {
   localStorage.setItem('tutorial_complete', 'yes');
@@ -144,7 +68,7 @@ export const TutorialDialog: FC<TutorialDialogProps> = ({
           numVisible={1}
           showNavigators={false}
           value={slides}
-          itemTemplate={WelcomeDialogSlide}
+          itemTemplate={TutorialSlide}
           onPageChange={({ page }) => setSlideNum(page)}
         />
       </div>
@@ -190,22 +114,3 @@ export const TutorialDialog: FC<TutorialDialogProps> = ({
     </Dialog>
   );
 };
-
-interface TutorialSlideProps {
-  title?: string;
-  image: string;
-  text: ReactNode;
-  id?: string;
-}
-
-const WelcomeDialogSlide: FC<TutorialSlideProps> = (props) => (
-  <div id={props.id} className="slide">
-    {props.title && <div className="slide-title">{props.title}</div>}
-
-    <div className="slide-image-box">
-      <img src={props.image} />
-    </div>
-
-    <p className="slide-text">{props.text}</p>
-  </div>
-);
