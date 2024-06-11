7e7c516f67202f156859664f701d9e48d1a8bcd1
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-06-11 22:04:19 +1000

    hotfix: update admin panel auth

--- apps/admin/src/app/providers.tsx
+++ apps/admin/src/app/providers.tsx
@@ -10,6 +10,8 @@ import { mainnet, sepolia } from 'viem/chains';
 import theme from '@/theme';
 import { baseUrl } from '@/config';
 import DynamicWrapper from '@/components/DynamicWrapper';
+import { ReactNode } from 'react';
+import { useDynamicContext } from '@dynamic-labs/sdk-react-core';
 
 const config = createConfig({
   chains: [mainnet, sepolia],
@@ -20,36 +22,50 @@ const config = createConfig({
   },
 });
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      queryFn: async ({ queryKey: [url, responseType] }) => {
-        if (typeof url === 'string') {
-          const { data } = await axios.get(`${baseUrl}/${url.toLowerCase()}`, {
-            responseType: (responseType as ResponseType) ?? undefined,
-          });
+export const QueryProvider = ({ children }: { children: ReactNode }) => {
+  const { authToken } = useDynamicContext();
 
-          if (responseType === 'blob') {
-            return new Blob([data]);
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        queryFn: async ({ queryKey: [url, responseType] }) => {
+          if (typeof url === 'string') {
+            const { data } = await axios.get(
+              `${baseUrl}/${url.toLowerCase()}`,
+              {
+                headers: authToken
+                  ? { Authorization: `Bearer ${authToken}` }
+                  : undefined,
+                responseType: (responseType as ResponseType) ?? undefined,
+              },
+            );
+
+            if (responseType === 'blob') {
+              return new Blob([data]);
+            }
+
+            return data;
           }
-
-          return data;
-        }
-        throw new Error('Invalid QueryKey');
+          throw new Error('Invalid QueryKey');
+        },
       },
     },
-  },
-});
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <DynamicWrapper>
       <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
+        <QueryProvider>
           <DynamicWagmiConnector>
             <ChakraProvider theme={theme}>{children}</ChakraProvider>
           </DynamicWagmiConnector>
-        </QueryClientProvider>
+        </QueryProvider>
       </WagmiProvider>
     </DynamicWrapper>
   );
