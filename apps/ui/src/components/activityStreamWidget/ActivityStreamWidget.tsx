bf591d3597e0481c962a8e918d2ba422c7064c84
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-06-12 10:14:15 +1000

    feat: AS message order
    - Also added smooth scrolling rather than "jumps".
    - Dynamically adjusts scroll threshold so that if the previous 'latest' message was in view before state change, then it scrolls (smoothly so you can still read what is moving). Other wise not.

--- apps/ui/src/components/activityStreamWidget/ActivityStreamWidget.tsx
+++ apps/ui/src/components/activityStreamWidget/ActivityStreamWidget.tsx
@@ -2,25 +2,24 @@ import { FC, useRef, useEffect } from 'react';
 import { useAppState } from '@/hooks';
 import { useActivityStream } from './useActivityStream';
 
-const AUTO_SCROLL_THRESHOLD = 25; //px
-
 export const ActivityStreamWidget: FC = () => {
   const { match } = useAppState();
   const { messages } = useActivityStream(match?.series, match?.matchId);
+  const lastMessageRef = useRef<HTMLDivElement>(null);
   const chatViewportRef = useRef<HTMLDivElement>(null);
   const prevScrollHeight = useRef<number>(0);
 
   const handleScroll = () => {
-    if (chatViewportRef.current) {
-      const { scrollTop, scrollHeight } = chatViewportRef.current;
+    console.log('trigger scroll');
+    if (chatViewportRef.current && lastMessageRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = chatViewportRef.current;
       const scrollDiff =
         prevScrollHeight.current === 0
           ? 0
           : scrollHeight - prevScrollHeight.current;
-      if (scrollTop <= AUTO_SCROLL_THRESHOLD) {
-        chatViewportRef.current.scrollTop = 0;
-      } else {
-        chatViewportRef.current.scrollTop = scrollTop + scrollDiff;
+      if (scrollHeight - (scrollTop + clientHeight) <= 2 * scrollDiff) {
+        // scroll if previous message was visible in previous state
+        lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
       }
       prevScrollHeight.current = scrollHeight;
     }
@@ -42,8 +41,12 @@ export const ActivityStreamWidget: FC = () => {
         {messages.length > 0 && (
           <div className="chat-viewport" ref={chatViewportRef}>
             <div className="chat-body">
-              {messages.toReversed().map((msg, index) => (
-                <div key={index} className="chat-message">
+              {messages.map((msg, index) => (
+                <div
+                  key={index}
+                  ref={index === messages.length - 1 ? lastMessageRef : null}
+                  className="chat-message"
+                >
                   <span className="chat-message-timestamp text-primary">
                     [{msg.timestamp}]
                   </span>
