2d1ee15d5614ce376a072c77a02b59d266a305a2
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-07-26 14:23:05 +0800

    Update tournament progress display to match designs (#126)
    * Update tournament progress

--- apps/ui/src/components/stagedProgressBar/StagedProgressBar.tsx
+++ apps/ui/src/components/stagedProgressBar/StagedProgressBar.tsx
@@ -20,6 +20,7 @@ export const StagedProgressBar: FC<StagedProgressBarProps> = ({
         <div className="staged-progress-bar-body">
           {stages.map((stage, index) => {
             const isCurrentStage = index === currentStage;
+            const isLastStage = index === stages.length - 1;
             const state = isCurrentStage
               ? 'current'
               : index < currentStage
@@ -27,28 +28,31 @@ export const StagedProgressBar: FC<StagedProgressBarProps> = ({
                 : 'future';
             const transitionProgress = isCurrentStage ? progress : 0;
             return (
-              <>
-                <div
-                  key={`stage-${index}`}
-                  ref={(el) => {
-                    isCurrentStage &&
-                      el?.scrollIntoView({
-                        behavior: 'smooth',
-                        block: 'nearest',
-                        inline: 'center',
-                      });
-                  }}
-                >
-                  <Stage state={state}>{stage}</Stage>
+              <div
+                key={`stage-${index}`}
+                ref={(el) => {
+                  isCurrentStage &&
+                    el?.scrollIntoView({
+                      behavior: 'smooth',
+                      block: 'nearest',
+                      inline: 'center',
+                    });
+                }}
+                className='day-box-bar'
+              >
+                <div className={`day-box ${isLastStage ? "flag-box" : ""}`}>
+                  <Stage state={state}>
+                    {isCurrentStage ? `Day ${stage}` : stage}
+                  </Stage>
+                  {isCurrentStage && (
+                    <StageTransition
+                      key={`transition-${index}`}
+                      state={state}
+                      progress={transitionProgress}
+                    />
+                  )}
                 </div>
-                {index !== stages.length - 1 && (
-                  <StageTransition
-                    key={`transition-${index}`}
-                    state={state}
-                    progress={transitionProgress}
-                  />
-                )}
-              </>
+              </div>
             );
           })}
         </div>
