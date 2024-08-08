c27fce0f8b16c7b17bdcf4177f94474491c3bdcf
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-08-08 15:35:18 +0800

    Clean up tournament prizes design and add prev winners (#141)

--- apps/ui/src/components/prizesWidget/PrizesWidget.tsx
+++ apps/ui/src/components/prizesWidget/PrizesWidget.tsx
@@ -8,19 +8,19 @@ const prizes: {
   title: string;
   description: string;
 }[] = [
-    {
-      title: 'NFT name',
-      description: 'Raffled to the top 20,000 XP Players',
-    },
-    {
-      title: 'NFT name',
-      description: 'Raffled to the top 20,000 XP Players',
-    },
-    {
-      title: 'NFT name',
-      description: 'Raffled to the top 20,000 XP Players',
-    },
-  ];
+  {
+    title: 'NFT name',
+    description: 'Raffled to the top 20,000 XP Players',
+  },
+  {
+    title: 'NFT name',
+    description: 'Raffled to the top 20,000 XP Players',
+  },
+  {
+    title: 'NFT name',
+    description: 'Raffled to the top 20,000 XP Players',
+  },
+];
 
 export interface PrizesWidgetProps {
   title: string;
@@ -37,6 +37,7 @@ export const PrizesWidget: FC<PrizesWidgetProps> = ({
   prizes,
 }) => {
   const [isReady, setReady] = useState(false);
+  const [showPrev, setShowPrev] = useState(false);
   const carouselRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => setReady(true));
@@ -54,14 +55,17 @@ export const PrizesWidget: FC<PrizesWidgetProps> = ({
         </div>
 
         <p className="widget-info">
-          Top 3 by XP win main prizes. All players enter raffle (1 XP = 1 entry). XP resets each tournament.
+          Top 3 by XP win main prizes. All players enter raffle (1 XP = 1
+          entry). XP resets each tournament.
         </p>
       </div>
 
       <div className="widget-body">
         <div>
           <div
-            className={classNames('prize-carousel', 'layout', { loading: !isReady })}
+            className={classNames('prize-carousel', {
+              loading: !isReady,
+            })}
             ref={carouselRef}
           >
             <PrizeTile
@@ -94,10 +98,99 @@ export const PrizesWidget: FC<PrizesWidgetProps> = ({
           <PrizeTile
             place="4"
             imageSrc="/raffle.svg"
-            // value={prizes[3]?.title}
             description={prizes[3]?.description}
             className="prize-tile"
           />
+        </div>
+
+        {showPrev ? (
+          <p className="top-header" onClick={() => setShowPrev(!showPrev)}>
+            Hide Previous winners
+            <img src="/arrow-down.svg" alt="" />
+          </p>
+        ) : (
+          <p className="top-header" onClick={() => setShowPrev(!showPrev)}>
+            Show Previous winners
+            <img src="/arrow-right.svg" alt="" />
+          </p>
+        )}
+        <div
+          className={classNames('prev-winner', {
+            collapsed: !showPrev,
+          })}
+        >
+          <div className="prev-winner-section">
+            <div className="prev-winner-card">
+              <div className="left">
+                <img src="/prizes/1st.svg" alt="" />
+                <p>Username</p>
+                <div className="middle-left">
+                  <span className="username">username</span>
+                  <span className="left-xp">2890 XP</span>
+                </div>
+              </div>
+              <div className="middle">
+                <p className="mobile-xp">2890 XP</p>
+              </div>
+              <div className="weighted-font">$2,500 USDC</div>
+            </div>
+
+            <div className="prev-winner-card">
+              <div className="left">
+                <img src="/prizes/2nd.svg" alt="" />
+                <p>Username</p>
+                <div className="middle-left">
+                  <span className="username">username</span>
+                  <span className="left-xp">2890 XP</span>
+                </div>
+              </div>
+              <div className="middle">
+                <p className="mobile-xp">2890 XP</p>
+              </div>
+              <div className="weighted-font">$1,000 USDC</div>
+            </div>
+
+            <div className="prev-winner-card">
+              <div className="left">
+                <img src="/prizes/3rd.svg" alt="" />
+                <p>Username</p>
+                <div className="middle-left">
+                  <span className="username">username</span>
+                  <span className="left-xp">2890 XP</span>
+                </div>
+              </div>
+              <div className="middle">
+                <p className="mobile-xp">2890 XP</p>
+              </div>
+              <div className="weighted-font">$500 USDC</div>
+            </div>
+
+            <div className="prev-winner-card">
+              <div className="left">
+                <img src="/prizes/raffle.svg" alt="" />
+                <p>20 winners</p>
+                <div className="middle-left">
+                  <span className="username">20 winners</span>
+                  <span className="view-all">view all</span>
+                </div>
+              </div>
+              <div className="middle">
+                <p className="view-all-mobile">view all</p>
+              </div>
+              <div className="weighted-font">$1,000 USDC</div>
+            </div>
+          </div>
+
+          <div className="prize-pool-panel">
+            <p className="text">Total Prize Pools</p>
+            <div className="prize-pool">
+              <span className="top">
+                <p className="amount">$5,000</p>
+                <p>USDC</p>
+              </span>
+              <p>Totally distributed during all of the tournaments</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -129,7 +222,7 @@ const PrizeTile: FC<PrizeTileProps> = (props) => {
       )}
 
       <div className="prize-info">
-        {props.value && (<div className="prize-value">{props.value}</div>)}
+        {props.value && <div className="prize-value">{props.value}</div>}
         <div className="prize-description">{props.description}</div>
       </div>
     </div>
