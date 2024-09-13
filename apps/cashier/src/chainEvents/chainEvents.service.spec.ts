dbcba6ed2522c1be0332dd7bab41b5379ce9dba9
Deepanshu Singh <dnshu.skynet@gmail.com>
2024-09-13 13:59:06 +1000

    hotfix: fix unit tests

--- apps/cashier/src/chainEvents/chainEvents.service.spec.ts
+++ apps/cashier/src/chainEvents/chainEvents.service.spec.ts
@@ -25,6 +25,10 @@ describe('ChainEventsService', () => {
           },
         },
         {
+          provide: 'WithdrawalsConnectedEventStore',
+          useValue: {},
+        },
+        {
           provide: ReadModelService,
           useValue: {},
         },
